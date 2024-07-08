b38e959bc4fda745b4fb2a5a369d353229ed5954
SPC <SPC@17-LAB402>
2024-07-08 18:58:22 +0700

    Add project files.

---
+++ CPP _ Dequy.cpp
@@ -0,0 +1,40 @@
+// CPP _ Dequy.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+// Hàm ?? quy tính t?ng các s? t? 1 ??n n
+int nSum(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    else {
+        return n + nSum(n - 1);
+    }
+}
+
+int main() {
+    int n;
+    cout << "Nhap n: ";
+    cin >> n;
+
+    // G?i hàm ?? quy ?? tính t?ng
+    int sum = nSum(n);
+
+    // In k?t qu?
+    cout << "Tong cac so tu 1 den n " << n << " la: " << sum << endl;
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
